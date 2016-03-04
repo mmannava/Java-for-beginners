3d55af432aa8318efc73dcde88cb3cd42a113de6
mmannava <manasapriyamvadamannava@yahoo.com>
2016-03-04 13:45:28 -0700

    Get introduced , Say Hello !

---
+++ Chapter1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Manasa on 3/4/2016.
+ *This program is probably the first java program that any beginner would write
+ */
+
+//
+public class SayHello {
+    public static void main(String args[]){
+        System.out.println("Hello & Welcome to the awesome world of java");
+    }
+}
