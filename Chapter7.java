aeae3a4da4c96db0155b92eb907c42b9e677f954
mmannava <manasapriyamvadamannava@yahoo.com>
2016-03-04 14:46:33 -0700

    Scan user input

---
+++ Chapter7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by Manasa on 3/4/2016.
+ * Lets greet each other
+ *
+ */
+public class Chapter7 {
+    public static void main(String args[]){
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello "+sc.next()+". I am Manasa. "+ "Nice to meet you.");
+
+
+    }
+}
