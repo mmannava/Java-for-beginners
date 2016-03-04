f5d9f44e7d5cc37ff0709b77329e5bfd0cc609a6
mmannava <manasapriyamvadamannava@yahoo.com>
2016-03-04 14:02:58 -0700

    String declaration and usage

---
+++ Chapter3.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Manasa on 3/4/2016.
+ * This chapter lets you understand how to declare strings and output them.
+ *
+ */
+public class Chapter3 {
+    public static void main(String args[]){
+        String fname = "Manasa";
+        String mname = "Priyamvada";
+        String lname = "Mannava";
+        System.out.println("My name is:"+" "+fname+" "+mname+" "+lname);
+
+    }
+}
