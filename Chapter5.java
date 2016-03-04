da2a5665c0a109eb097c986c04fbed10c9e9ff85
mmannava <manasapriyamvadamannava@yahoo.com>
2016-03-04 14:22:28 -0700

    Ex illustrating for loop

---
+++ Chapter5.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Manasa on 3/4/2016.
+ * Lets play hide and seek again, but this time I will use a for loop to count.
+ */
+public class Chapter5 {
+    public static void main(String args[]){
+        for(int i=1;i<=10;i++){
+            System.out.println("count:"+i);
+        }
+        System.out.println("I am done counting till 10 and I am  beginning to search");
+    }
+}
