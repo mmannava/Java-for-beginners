ac7c1055deebb1e45d1afd711734e8df90486ff2
mmannava <manasapriyamvadamannava@yahoo.com>
2016-03-04 15:31:22 -0700

    Ex to illustrate do-while loop

---
+++ Chapter8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by Manasa on 3/4/2016.
+ */
+public class Chapter8 {
+    static boolean value = false;
+    public static void main(String args[]){
+
+        do{
+            System.out.println("Do executes atleast once, until the condition is false");
+        }while(value);
+    }
+}
