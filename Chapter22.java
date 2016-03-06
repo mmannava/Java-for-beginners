77b324f3418c91f9498f56a7e2c75e62438e831d
mmannava <manasapriyamvadamannava@yahoo.com>
2016-03-05 20:28:37 -0700

    Ex to illustrate interface

---
+++ Chapter22.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Manasa on 3/5/2016.
+ *
+ * The class Chapter22a implements the interface
+ *
+ */
+
+
+public interface Chapter22{
+    public  void displayName();
+
+}
+
+class Chapter22a implements Chapter22{
+    public void displayName(){
+        System.out.println("DisplayName:"+" Manasa");
+    }
+}
